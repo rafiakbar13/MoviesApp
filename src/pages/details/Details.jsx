5e9e8febb54ffa643ebd0e8cda5ddd0cb1160a99
rafiakbar13 <muhammadrafia13@gmail.com>
2023-05-13 21:52:04 +0700

    modified details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import useFetch from '../../hooks/useFetch'
-import { useParams } from 'react-router-dom'
-import DetailsBanner from './detailsBanner/DetailBanner'
-import Cast from './cast/Cast'
-import VideosSection from './videosSection/VideosSection'
-import Similar from './carousels/Similar'
-import Recommendation from './carousels/Recommendation'
-
+import React from "react";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
-  const {mediaType, id} = useParams()
+  const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const {data: credits, loading: creditsLoading} = useFetch(`/${mediaType}/${id}/credits`)
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
   return (
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
@@ -20,7 +21,7 @@ const Details = () => {
       <Recommendation mediaType={mediaType} id={id} />
       <Similar mediaType={mediaType} id={id} />
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
